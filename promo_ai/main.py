d59b81ee6c88d3cd35a41abf5234263fcbb1032d
edwardheaver <ed.heaver@justpark.com>
2024-05-23 14:02:06 +0100

    Chat gave the right thing!!
    Needs further prompting to make sure it doesn't guess stuff and inputs the right data, but we're winning

--- promo_ai/main.py
+++ promo_ai/main.py
@@ -1,4 +1,18 @@
-
+from llama_index.llms.ollama import Ollama
+from llama_index.core.llms import ChatMessage, MessageRole
+from promo_ai.prompts import full_prompt
 
 def main():
-    print("hello world")
+    llm = Ollama(model="codellama", request_timeout=120)
+
+    first = ChatMessage(role=MessageRole.SYSTEM, content=full_prompt)
+    second = ChatMessage(role=MessageRole.USER, content="Create a promo code that gives me £5 off my first booking")
+
+    messages = [
+        first,
+        second
+    ]
+
+    res = llm.chat(messages)
+
+    print(res)
