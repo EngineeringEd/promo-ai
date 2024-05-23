d89002b339c23c10689238ef6084d3d348ef29c2
edwardheaver <ed.heaver@justpark.com>
2024-05-23 17:30:49 +0100

    helper to convert messages

--- promo_ai/chat_app.py
+++ promo_ai/chat_app.py
@@ -1,3 +1,4 @@
+from typing import Any, Dict
 import streamlit as st
 
 from llama_index.core.llms import ChatMessage, MessageRole
@@ -15,3 +16,12 @@ for msg in st.session_state["messages"]:
 if prompt := st.chat_input():
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
+
+
+
+def convert_chat_message(msg: ChatMessage) -> Dict[str, Any]:
+    match msg.role:
+        case MessageRole.ASSISTANT:
+            return {"role": "Assistant", "content": msg.content}
+        case _:
+            return {"role": "User", "content": msg.content}
